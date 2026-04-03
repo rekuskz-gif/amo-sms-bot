a18eeb7f619445b832a6a3a65a4aba19aca84d8c
rekuskz-gif <rekuskz@gmail.com>
2026-04-03 19:52:06 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -74,6 +74,7 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
+        
         self.send_header("Content-Type", "application/json")
         self.end_headers()
         self.wfile.write(json.dumps({"status": "amo-sms-bot running"}).encode())
